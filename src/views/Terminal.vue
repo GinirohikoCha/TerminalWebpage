c8e1e30144162893df1ab9a551adc62ee289f31d
GinirohikoCha <rinkouatf@qq.com>
2022-06-22 17:29:52 +0800

    🚧 add Terminal.vue & general style

---
+++ src/views/Terminal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="terminal-container">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+})
+
+export default class Terminal extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+.terminal-container {
+  width: 100%;
+  height: 100%;
+  background-color: black;
+}
+</style>
